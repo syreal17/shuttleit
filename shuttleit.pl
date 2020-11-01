79372afbb9245422948aa29255a631bdb38fbbe6
syreal17 <ltjones@acm.org>
2020-11-01 00:46:13 -0400

    Modify button 5 to be middle mouse button

--- shuttleit.pl
+++ shuttleit.pl
@@ -150,7 +150,8 @@ sub process_state {
 	system "/usr/bin/xdotool key XF86AudioPrev" if $state->{down2};
 	system "/usr/bin/xdotool key XF86AudioPlay" if $state->{down3};
 	system "/usr/bin/xdotool key XF86AudioNext" if $state->{down4};
-	system "/usr/bin/xdotool key alt+shift+w" if $state->{down5};
+	#system "/usr/bin/xdotool key alt+shift+w" if $state->{down5};
+	system "/usr/bin/xdotool click 2" if $state->{down5};
 	
 	# Stop if hold down button 1 + press button 3
 	system "/usr/bin/xdotool key XF86AudioStop" if ($state->{btn1} && $state->{down3});
